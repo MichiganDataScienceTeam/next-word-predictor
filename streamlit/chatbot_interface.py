7459d02f60ecbcbe33987cfccb8f6686672ce51c
Amirali Danai <danaiamirali@gmail.com>
2023-11-13 13:42:56 -0500

    Update Streamlit

--- streamlit/chatbot_interface.py
+++ streamlit/chatbot_interface.py
@@ -1,36 +1,101 @@
 import streamlit as st
-import tensorflow as tf
+import numpy as np
 import pickle
+import tensorflow as tf
+import nltk
 
-# Adjust as necessary
-model = tf.keras.models.load_model('../model.h5')
-index_to_word = pickle.load(open('index_to_word.pkl', 'rb'))
-word_to_index = pickle.load(open('word_to_index.pkl', 'rb'))
+nltk.download('punkt')
 
-MAX_SEQUENCE_LEN = 122
+st.markdown("""
+<style>
+	[data-testid="stHeader"] {
+		background-image: linear-gradient(90deg, rgb(57, 163, 161), rgb(170, 115, 199));
+	}
+</style>""",
+unsafe_allow_html=True)
 
-### USAGE: python -m streamlit run chatbot_interface.py
+# Load dictionaries
+file = open("exports/amzn_idx_to_word.pkl", 'rb')
+idx_to_word = pickle.load(file)
+file.close()
+file = open("exports/amzn_word_to_idx.pkl", 'rb')
+word_to_idx = pickle.load(file)
+file.close()
+max_sequence_len = 199
 
-def predict_next_word(input: str) -> str:
-    input_token = [word_to_index[word] if word in word_to_index else 0 for word in input.split()]
-    input_token = tf.keras.preprocessing.sequence.pad_sequences([input_token], maxlen=MAX_SEQUENCE_LEN, padding='pre')
-    prediction = model.predict(input_token)
+model = tf.keras.models.load_model('exports/amazon_reviews_GRU.h5')
 
-    return index_to_word[prediction.argmax()]
+def predict_next_n_words(model, text, n, max_sequence_len, word_to_index, index_to_word):
+    """
+    Predict the next n words based on the input text.
 
-if __name__ == "__main__":
-    st.title("Next Word Predictor") 
+    Args:
+    - model (tf.keras.Model): Trained model for prediction.
+    - text (str): Input string.
+    - n (int): Number of words to predict.
+    - max_sequence_len (int): Maximum length of input sequences.
+    - word_to_index (dict): Mapping from words to their respective indices.
+    - index_to_word (dict): Mapping from indices to their respective words.
 
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "assistant", "content": "Type in a sequence of words, I'll predict the next word for you!"}]
+    Returns:
+    - str: Predicted sequence of words.
+    """
 
-    for msg in st.session_state.messages:
+    predicted_sequence = []
+
+    for _ in range(n):
+        # Tokenize the input string
+        token_list = [word_to_index[word] for word in nltk.tokenize.word_tokenize(text) if word in word_to_index]
+
+        # Pad the token sequence
+        token_list = tf.keras.utils.pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+
+        # Predict the token of the next word
+        predicted_idx = np.argmax(model.predict(token_list), axis=-1)
+
+        # Convert the token back to a word
+        predicted_word = index_to_word.get(predicted_idx[0], '')
+
+        # Append the predicted word to the sequence and to the text (for the next iteration)
+        predicted_sequence.append(predicted_word)
+        text += " " + predicted_word
+
+    return ' '.join(predicted_sequence)
+
+with st.sidebar:
+    st.markdown(
+        "<h1>About Our Model</h1>",
+        unsafe_allow_html=True,
+    )
+    "Natural Language Processing model that predicts the next 'n' words, specified by the user, given a user input."
+    st.divider()
+    st.markdown(
+        "<h3>Designed by Michigan Data Science Team in Fall 2023</h3>",
+        unsafe_allow_html=True,
+    )
+    one, two = st.columns(2)
+    with one: 
+        st.image("media/MDST_logo.jpg", width= 200)
+
+st.title("➱ Next Word Predictor") 
+st.divider()
+number = st.slider("Pick a number of words to predict", 1, 100)
+st.divider()
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "Hey there!👋 What sequence would you like to predict?"}]
+
+for msg in st.session_state.messages:
+    if msg["role"] == "user":
+        st.chat_message(msg["role"], avatar=("media/userImg.jpg")).write(msg["content"])
+    else:
         st.chat_message(msg["role"]).write(msg["content"])
 
-    if prompt := st.chat_input():
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.chat_message("user").write(prompt)
-        msg = {"role": "assistant", "content": predict_next_word(prompt)}
-        st.session_state.messages.append(msg)
-        st.chat_message("assistant").write(msg['content'])
-    
+count = 0
+
+if prompt := st.chat_input("Enter your message here", key = count):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user", avatar=("media/userImg.jpg")).write(prompt)
+    response = predict_next_n_words(model, prompt, number, max_sequence_len, word_to_idx, idx_to_word)
+    st.session_state.messages.append({"role": "assistant", "content": prompt + " " + response})
+    st.chat_message("assistant").write(prompt + " " + response)
+    count += 1
